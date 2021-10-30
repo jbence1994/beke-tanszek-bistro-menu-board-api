18e837aa2d14f7104ade56b2c047887c16cfa697
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-30 12:32:43 +0200

    Seed types table

---
+++ BekeTanszekBistro.MenuBoard.Api/Migrations/20211030102944_SeedTypesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BekeTanszekBistro.MenuBoard.Api.Migrations
+{
+    public partial class SeedTypesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Levesek')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Főételek')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Vegetáriánus')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Frissensültek')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Tészták')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Köretek')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Desszert')");
+            migrationBuilder.Sql("INSERT INTO types (name) VALUES ('Saláták')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM types;");
+        }
+    }
+}
