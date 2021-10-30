c60d6057020160b5ebfa47129f64301c1a3de1ae
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-30 12:29:22 +0200

    Describe Type model and migrate database table

---
+++ BekeTanszekBistro.MenuBoard.Api/Core/Models/Type.cs
@@ -0,0 +1,8 @@
+﻿namespace BekeTanszekBistro.MenuBoard.Api.Core.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BekeTanszekBistro.MenuBoard.Api/Migrations/20211030102719_AddTypesTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace BekeTanszekBistro.MenuBoard.Api.Migrations
+{
+    public partial class AddTypesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_types", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "types");
+        }
+    }
+}
