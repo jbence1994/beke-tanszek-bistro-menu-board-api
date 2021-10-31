0a9de08667d85fd739973b2164005d8f38b82b75
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-10-31 11:08:42 +0100

    Add DateTimeHelper

---
+++ BekeTanszekBistro.MenuBoard.Api/Helpers/DateTimeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BekeTanszekBistro.MenuBoard.Api.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static bool Equals(DateTime date)
+        {
+            var now = DateTime.Now;
+
+            return now.Year == date.Year &&
+                   now.Month == date.Month &&
+                   now.Day == date.Day;
+        }
+    }
+}
