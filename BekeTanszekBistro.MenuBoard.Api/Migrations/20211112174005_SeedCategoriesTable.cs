5b573f3286608a9dd0ba7d1f4e398ac261b5680e
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-11-12 18:44:11 +0100

    Seed categories table

---
+++ BekeTanszekBistro.MenuBoard.Api/Migrations/20211112174005_SeedCategoriesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BekeTanszekBistro.MenuBoard.Api.Migrations
+{
+    public partial class SeedCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Leves')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Főétel')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Frissensült')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Köret')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Tészták')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Vega / Érzékenyek')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Saláták / Dresszing')");
+            migrationBuilder.Sql("INSERT INTO categories (name) VALUES ('Extra ajánlat')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categories;");
+        }
+    }
+}
